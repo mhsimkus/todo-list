3b52ca6cfe3d30bb2dd3a6ef3fbc6365fcc59058
Megan Simkus <150747745+mhsimkus@users.noreply.github.com>
2024-03-09 09:03:50 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
 function App(){
-  const [todos, setTodos] = React.useState([
-    {
-      text: 'learn react',
-      isCompleted: false,
-    },
-    {
-      text: 'meet friend for lunch',
-      isCompleted: false,
-    },
-    {
-      text: 'build todo app',
-      isCompleted: false,
-    }        
-  ])
+    const [todos, setTodos] = React.useState([
+      {
+        text: 'learn react',
+        isCompleted: false,
+      },
+      {
+        text: 'meet friend for lunch',
+        isCompleted: false,
+      },
+      {
+        text: 'build todo app',
+        isCompleted: false,
+      }        
+    ]);
+  const [value, setValue] = React.useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    const newTodos = [...todos, {text:value, isCompleted:false}];
+    setTodos(newTodos);
+    setValue('');
+  }
+    return(<>
+        {todos.map((todo,i) => 
+          <div className="todo" key={i}>{todo.text}</div>)}
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="input"
+            value={value}
+            placeholder="Add Todo..."
+            onChange={e => setValue(e.target.value)}
+            />
+        </form>
+      </>);
+  }
+  
+  ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+  )
 
-  return(
-    <>
-      {todos.map((todo, i) => <div key={i}>{todo.text}</div>)}
-    </>
-  );
-}
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
-);
